05d6c6a19ea04ea776e949cdca75bb8755dd9c31
Kevin Nishimura <kevin@vdinetworks.com>
2019-03-25 17:22:11 -0400

    added duo config file

---
+++ PSDuo/PSDuo.Settings.ps1
@@ -0,0 +1,16 @@
+#PSDuo Configuration File
+
+ @{
+        prod = @{
+                iKey  = [string]"DIxxxxxxxxxxxxxxxxxx"
+                sKey = [string]"YourSecretsHere"
+                apiHost = [string]"api-nnnnnxnx.duosecurity.com"
+                directory_key = [string]"XXXXXXXXXXXXXXXXXXXX"
+                }
+        test = @{
+                iKey  = [string]"DIxxxxxxxxxxxxxxxxxx"
+                sKeyEnc = [string]"Big Long protected string on 1 line here"
+                apiHost = [string]"api-nnnnnxnx.duosecurity.com"
+                directory_key = [string]"XXXXXXXXXXXXXXXXXXXX"
+                }
+    }
